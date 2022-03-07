8540b1bd9f975ce01ed5bbf8baa59c0f6f9ecccc
Pariwesh <tamrpariwesh@gmail.com>
2022-03-07 16:04:28 +1100

    fetched and rendered movie list to home screen

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { getAllMovies } from '../../redux/Movies/MovieSlice'
+import MovieCard from '../MovieCard/MovieCard'
+import './MovieList.scss'
 
 const MovieList = () => {
   const movies = useSelector(getAllMovies)
-  console.log(movies)
-  return <div>MovieList</div>
+  let renderMovies = ''
+
+  renderMovies =
+    movies.Response === 'True' ? (
+      movies.Search.map((movie, index) => (
+        <MovieCard key={index} data={movie} />
+      ))
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    )
+  return (
+    <div className="movies-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
+  )
 }
 
 export default MovieList
